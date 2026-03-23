1cb7660b14b3517d305cac9358617985f63c92ee
aman <kumarisaumya@Kumaris-MacBook-Air.local>
2026-03-23 06:28:18 +0530

    linear search with condition i+1%n is speacial algo that makes last element and first element as neighboor i.r circular array

--- patterns/binarysearch/binarysearch.js
+++ patterns/binarysearch/binarysearch.js
@@ -5,8 +5,8 @@ const bs = (arr, target )=>{
     while(left <= right){
         let mid = Math.floor((left+right)/2);
         if(arr[mid]===target) return mid;
-        else if (arr[mid > target]) right = mid-1;
-         else if (arr[mid < target]) left = mid+1;
+        else if (arr[mid] > target) right = mid-1;
+        else if (arr[mid]< target) left = mid+1;
     }
     return -1;
 }
