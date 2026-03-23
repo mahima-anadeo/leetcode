5e066ad99ce1cecc42221e9baa70cf0821842aee
aman <kumarisaumya@Kumaris-MacBook-Air.local>
2026-03-23 06:24:02 +0530

    linear search with condition i+1%n is speacial algo that makes last element and first element as neighboor i.r circular array

---
+++ patterns/linear-search-with-parity-check/issortedrotatedarray.js
@@ -0,0 +1,20 @@
+
+
+function isRotatedSortedArray(nums) {
+    let count = 0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] > nums[(i+1) % n ]){
+            count++;
+        }
+    }
+  
+    return count <=1;
+         
+}
+const rotatedSortedArray1 = [4, 5, 6, 7, 1, 2, 3];
+const rotatedSortedArray2 = [1, 2, 3, 4, 5, 6, 7];
+console.log(isRotatedSortedArray(rotatedSortedArray1))
+console.log(isRotatedSortedArray(rotatedSortedArray2))
+console.log(isRotatedSortedArray([2,1,3,4]))
+console.log(isRotatedSortedArray([2,1]))
+console.log(isRotatedSortedArray([1,1,1]))
